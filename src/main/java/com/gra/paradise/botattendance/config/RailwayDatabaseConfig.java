242b8edd4279b2d2c5674d9ac056440e6d41c0ca
Tiago Holanda <iamsunli155@gmail.com>
2025-07-09 17:43:43 -0300

    .

--- src/main/java/com/gra/paradise/botattendance/config/RailwayDatabaseConfig.java
+++ src/main/java/com/gra/paradise/botattendance/config/RailwayDatabaseConfig.java
@@ -1,5 +1,6 @@
 package com.gra.paradise.botattendance.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,22 +11,26 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
-@Profile("railway")
 public class RailwayDatabaseConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "DATABASE_URL")
     public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String dbUrl = System.getenv("DATABASE_URL");
+        if (dbUrl != null && !dbUrl.isEmpty()) {
+            URI dbUri = new URI(dbUrl);
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String url = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String url = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setUrl(url);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
 
-        return dataSource;
+            return dataSource;
+        }
+        return null;
     }
 }
